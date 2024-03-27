0c991f6c1901abab09515a189d9b2a951552b3b5
Preetikaus <119099310+Preetikaus@users.noreply.github.com>
2024-03-27 15:45:29 +0530

    Add files via upload

---
+++ backend/Day7/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.Zenyoga.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Zenyoga.web.constant.Api;
+import com.Zenyoga.web.dto.request.AuthenticationRequest;
+import com.Zenyoga.web.dto.request.RegisterRequest;
+import com.Zenyoga.web.dto.response.AuthenticationResponse;
+import com.Zenyoga.web.service.AuthService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(Api.AUTH)
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        boolean isRegistered = authService.userRegistration(request);
+        return isRegistered ? ResponseEntity.status(200).body("User registered successfully")
+                : ResponseEntity.badRequest().body("Something went wrong!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authService.userAuthentication(request));
+    }
+}
---
+++ backend/Day7/dto/request/OrderRequest.java
@@ -0,0 +1,21 @@
+package com.Zenyoga.web.dto.request;
+
+import java.util.List;
+
+import com.Zenyoga.web.dto.info.ProductInfo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private String orderAddress;
+    private String paymentMode;
+    private Long uid;
+    private List<ProductInfo> products;
+}
